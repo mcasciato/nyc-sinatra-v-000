e3b3a3e4a9ce5c71b4016efb104b10c8fcc94ff9
mcasciato <michael.casciato@icloud.com>
2016-06-07 17:50:12 -0400

    updated figures controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,5 +8,25 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
-  
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(params[:title])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figure/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
 end
