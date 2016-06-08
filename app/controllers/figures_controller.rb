84349e99a88d2ec7589a3040b4c158aba0f5219e
mcasciato <michael.casciato@icloud.com>
2016-06-08 09:29:13 -0400

    updated figures/edit in views

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,11 +8,6 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id])
-    erb :'/figures/show'
-  end
-
   post '/figures' do
     @figure = Figure.create(params[:figure])
     if !params[:landmark][:name].empty?
@@ -25,8 +20,26 @@ class FiguresController < ApplicationController
     redirect to "/figures/#{@figure.id}"
   end
 
-  get '/figure/:id/edit' do
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
     erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(params[:title])
+    end
+    @figure.save
+    redirect to "/figures/show}"
   end
 end
